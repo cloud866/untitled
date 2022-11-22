00996c1c538a209039f12057397f4b14d1f5c3e4
cloud866 <dklein927@gmail.com>
2022-11-22 19:02:19 +0200

    Initial commit

---
+++ src/Loader.java
@@ -0,0 +1,36 @@
+public class Loader {
+    public static void main(String[] args) {
+        public static void main(String[] args) {
+            char ch1 = 'h';
+            char ch2 = 'i';
+            char ch3 = 'b';
+            char ch4 = 'm';
+            char ch5 = 'k';
+
+            System.out.println(decode(ch1) + " " + decode(ch2) + " " + decode(ch3));
+        }
+
+        public static String decode(char ch) {
+
+            //Писать код здесь
+            switch (decode) {
+                ch1:
+                    System.out.println("Hello!");
+                    break;
+                ch2:
+                    System.out.println("I can decode!");
+                    break;
+                ch3:
+                    System.out.println("Bye!");
+                    break;
+                ch4:
+                    System.out.println("I can decode");
+                    break;
+                ch5:
+                    System.out.println("I can decode");
+                    break;
+                default:
+                    System.out.println("I dont know these symbols");
+            }
+        }
+    }
